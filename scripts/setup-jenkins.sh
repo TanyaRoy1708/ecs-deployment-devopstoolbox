ce53f774574d107ab42979e48db115ce959520d2
TanyaRoy1708 <roytanya2800@gmail.com>
2026-05-15 18:12:26 +0530

    updated versions

--- scripts/setup-jenkins.sh
+++ scripts/setup-jenkins.sh
@@ -9,7 +9,7 @@ sudo apt-get update -y
 sudo apt-get install -y fontconfig openjdk-21-jre curl unzip git software-properties-common
 
 # 2. Add Jenkins Repo and install Jenkins
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+sudo curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install -y jenkins
